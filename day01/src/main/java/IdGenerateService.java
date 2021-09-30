d384a0d66281f6793dead641c0fed0b7e3fa95a4
up1 <somkiat.p@gmail.com>
2021-09-30 16:35:14 +0700

    Working with stub

--- day01/src/main/java/IdGenerateService.java
+++ day01/src/main/java/IdGenerateService.java
@@ -2,11 +2,14 @@ import java.util.Random;
 
 public class IdGenerateService {
 
+    private Random random;
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
     public String getData() {
-        // Dependency
-        Random random = new Random();
         int id = random.nextInt(10);
-        // Result
         return "CODE" + id;
     }
 
--- day01/src/test/java/IdGenerateServiceTest.java
+++ day01/src/test/java/IdGenerateServiceTest.java
@@ -1,12 +1,24 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class IdGenerateServiceTest {
+class Random6 extends Random {
+    @Override
+    public int nextInt(int bound) {
+        return 6;
+    }
+}
 
+public class IdGenerateServiceTest {
     @Test
     public void getData() {
-        IdGenerateService service = new IdGenerateService();
+        Random stub = new Random6(); // Dependency
+        IdGenerateService service
+                = new IdGenerateService();
+        // 2. Setter/field/property Injection
+        service.setRandom(stub);
         String result = service.getData();
         assertEquals("CODE6", result);
     }
