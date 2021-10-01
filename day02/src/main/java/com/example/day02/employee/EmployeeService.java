4010dc760be0c2c1e4bfc69e8a9f297a25edd781
up1 <somkiat.p@gmail.com>
2021-10-01 16:51:17 +0700

    Integrate repository into service layer

--- day02/src/main/java/com/example/day02/employee/EmployeeService.java
+++ day02/src/main/java/com/example/day02/employee/EmployeeService.java
@@ -1,11 +1,22 @@
 package com.example.day02.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     public EmployeeResponse get(int id) {
         // TODO :: read data from database
-        return new EmployeeResponse(id, "somkiat");
+        Optional<Employee> result = employeeRepository.findById(id);
+        if(result.isPresent()) {
+            return new EmployeeResponse(id, "somkiat");
+        }
+        throw new RuntimeException("Employee not found with id=" + id);
     }
 }
