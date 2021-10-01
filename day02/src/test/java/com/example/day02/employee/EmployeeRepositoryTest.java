a6560da154bd7a969bd5b1bffd1fe8827f13bbbc
up1 <somkiat.p@gmail.com>
2021-10-01 16:40:14 +0700

    Repository testing

---
+++ day02/src/test/java/com/example/day02/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.day02.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    public void case01() {
+        // Arrange
+        Employee demo = new Employee();
+        demo.setName("somkiat");
+        employeeRepository.save(demo);
+        // Act
+        Optional<Employee> result = employeeRepository.findById(1);
+        // Assert
+        assertTrue(result.isPresent());
+        assertEquals(1, result.get().getId());
+        assertEquals("somkiat", result.get().getName());
+    }
+
+}
