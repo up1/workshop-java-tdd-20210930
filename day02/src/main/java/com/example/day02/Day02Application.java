4ffe86a7c838d3bc114260614a34c1d27ae12916
up1 <somkiat.p@gmail.com>
2021-10-01 13:44:36 +0700

    Coount bean of spring's context

--- day02/src/main/java/com/example/day02/Day02Application.java
+++ day02/src/main/java/com/example/day02/Day02Application.java
@@ -2,12 +2,15 @@ package com.example.day02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Day02Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day02Application.class, args);
+		ConfigurableApplicationContext context
+				= SpringApplication.run(Day02Application.class, args);
+		System.out.println(context.getBeanDefinitionCount());
 	}
 
 }
