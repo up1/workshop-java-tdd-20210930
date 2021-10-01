acee77a9013bba4bd8ca20c72e84badbfc91128d
up1 <somkiat.p@gmail.com>
2021-10-01 10:36:39 +0700

    Parameter testing

---
+++ day01/src/test/java/badcode/RegisterBusinessGetFeeTest.java
@@ -0,0 +1,28 @@
+package badcode;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RegisterBusinessGetFeeTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,500",
+            "1,500",
+            "2,250",
+            "3,250",
+            "4,100",
+            "5,100",
+            "8,50",
+            "9,50",
+            "10,0",
+    })
+    public void getFee(int exp, int fee) {
+        RegisterBusiness business = new RegisterBusiness();
+        int actualFee = business.getFee(exp);
+        assertEquals(fee, actualFee);
+    }
+}
