e36cf952f6e73e5681dae2e2366973a2c5064910
up1 <somkiat.p@gmail.com>
2021-10-01 15:40:22 +0700

    Working with mock bean

--- day02/src/test/java/com/example/day02/employee/EmployeeControllerWebMvcTest.java
+++ day02/src/test/java/com/example/day02/employee/EmployeeControllerWebMvcTest.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.assertj.core.api.BDDAssumptions.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,16 +22,24 @@ public class EmployeeControllerWebMvcTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private EmployeeService employeeService;
+
     @Test
     public void getById() throws Exception {
+        // Arrange
+        EmployeeResponse data = new EmployeeResponse(1, "XYZ");
+        when(employeeService.get(1)).thenReturn(data);
+        // Act
         MvcResult mvcResult
                 = mvc.perform(get("/employee/1")).andExpect(status().isOk()).andReturn();
+        // Assert
         ObjectMapper mapper = new ObjectMapper();
         EmployeeResponse result
                 = mapper.readValue(mvcResult.getResponse().getContentAsByteArray(),
                                    EmployeeResponse.class);
         // Assert
         assertEquals(1, result.getId());
-        assertEquals("somkiat", result.getName());
+        assertEquals("XYZ", result.getName());
     }
 }
