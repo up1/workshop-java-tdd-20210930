dbf11e4cf48cea2e62f98b091a595e4231157696
up1 <somkiat.p@gmail.com>
2021-10-01 12:09:01 +0700

    Create spting project

---
+++ day02/src/main/java/com/example/day02/Day02Application.java
@@ -0,0 +1,13 @@
+package com.example.day02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day02Application.class, args);
+	}
+
+}
---
+++ day02/src/test/java/com/example/day02/Day02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
